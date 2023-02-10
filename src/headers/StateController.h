02c9a65d7fd547a1c3e521f5f653d4f17e185747
Yarik Utkin <kryakkva@ya.ru>
2023-02-10 17:43:59 +0300

    add stateController class

---
+++ src/headers/StateController.h
@@ -0,0 +1,16 @@
+//
+// Created by Yarik on 10.02.2023.
+//
+
+#ifndef MLP__STATECONTROLLER_H_
+#define MLP__STATECONTROLLER_H_
+
+namespace s21 {
+
+class StateController {
+
+};
+
+} // s21
+
+#endif //MLP__STATECONTROLLER_H_
---
+++ src/sources/StateController.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Yarik on 10.02.2023.
+//
+
+#include "StateController.h"
+
+namespace s21 {
+} // s21
